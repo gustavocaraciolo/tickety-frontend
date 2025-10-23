0a8a3ed386afa1d435633bf2d1037d2fcb0c75ac
Gustavo Caraciolo <gustavocaraciolo@gmail.com>
2025-10-23 17:31:29 +0100

    Update PublicHeader to adjust logo size and add title for improved branding and visual hierarchy.

--- components/PublicLayout/PublicHeader/index.tsx
+++ components/PublicLayout/PublicHeader/index.tsx
@@ -20,14 +20,15 @@ const PublicHeader = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-20">
                     {/* Logo */}
-                    <Link href="/" className="flex items-center">
+                    <Link href="/" className="flex items-center space-x-1">
                         <Image
                             src="/images/logo.svg"
                             alt="Tickety"
-                            width={120}
+                            width={80}
                             height={40}
-                            className="h-8"
+                            className="h-10"
                         />
+                        <span className="text-2xl font-bold text-gray-900">Tickety</span>
                     </Link>
 
                     {/* Location Filter */}
